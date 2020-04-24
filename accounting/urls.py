382231f738a8266e51d80abc939ad85b694f6157
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-04-24 22:28:05 +0200

    add clienet thorugh frontend

--- accounting/urls.py
+++ accounting/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounting'
 urlpatterns = [
     path('', views.index, name='index'),
     path('clients/', views.clients, name='clients'),
+    path('clients/create', views.client_create, name='client_create'),
 ]
--- accounting/utils.py
+++ accounting/utils.py
@@ -5,3 +5,4 @@ def employee_check(request):
     if not hasattr(request.user, 'employee'):
         messages.add_message(request, messages.ERROR, 'User has no company assigned.')
         return False
+    return True

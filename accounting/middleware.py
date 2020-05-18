d410d2ade83fd654fbbd3a41b64fd0ff12739028
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-05-18 13:51:54 +0200

    fixed middleware for logging views

--- accounting/middleware.py
+++ accounting/middleware.py
@@ -1,21 +1,27 @@
-from .models import Invoice, InvoiceViewed
+from .models import Invoice, InvoiceViewed, Employee
 from django.utils.deprecation import MiddlewareMixin
+
 import re
 
 
 class InvoiceMiddleware(MiddlewareMixin):
     def process_request(self, request, *args, **kwargs):
-        url = request.META['PATH_INFO'].split('/')
+        url = request.path.split('/')
+        if request.user.is_anonymous:
+            return None
+        if Employee.objects.get(user=request.user) or request.user.is_staff:
+            print('x')
+            return None
         if len(url) < 4:
             return None
-        # path = url[3]
-        # if re.search("invoice", path):
-        #     print(path)
-        #     return None
+        path = url[3]
+        if not url[3] == "invoice":
+            return None
 
-        # invoice = Invoice.objects.get(uuid=url[4])
-        # print(invoice)
-        # invoice_viewed = InvoiceViewed.objects.get_or_create(invoice=invoice)
-        # invoice_viewed.save()
+        try:
+            invoice = Invoice.objects.get(uuid=url[4])
+            InvoiceViewed.objects.get_or_create(invoice=invoice)
+        except Invoice.DoesNotExist:
+            return None
 
         return None

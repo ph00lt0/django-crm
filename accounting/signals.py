693358a36cb0b2bdd5fe3ea6edae8f48584bf26f
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-06-03 19:44:08 +0200

    fix unique username bug for client and remove cdn's

--- accounting/signals.py
+++ accounting/signals.py
@@ -2,18 +2,20 @@ from django.db import transaction
 from django.db.models.signals import post_save, pre_save, pre_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import ClientAccount, Client, InvoiceSent
+from .models import ClientAccount, Client, ClientDetail, InvoiceSent
 from .tasks import send_email_invoice
 
 
-@receiver(post_save, sender=Client, dispatch_uid="create_client_account")
+@receiver(post_save, sender=ClientDetail, dispatch_uid="create_client_account")
 @transaction.atomic
 def create_client_account(sender, instance, **kwargs):
-    if not ClientAccount.objects.filter(client=instance).exists():
+    if not ClientAccount.objects.filter(client=instance.client).exists():
         user = User()
+        user.username = instance.email
+        user.email = instance.email
         user.save()
         client_account = ClientAccount()
-        client_account.client = instance
+        client_account.client = instance.client
         client_account.user = user
         client_account.save()
 
